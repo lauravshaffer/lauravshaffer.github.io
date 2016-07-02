3938911652d7d533061b076a4933c63fb7bb49d8
Laura Shaffer <lauravshaffer@gmail.com>
2016-07-02 15:26:34 -0500

    form update

--- submit.php
+++ submit.php
@@ -1,27 +1,65 @@
 <?php
-if(isset($_POST["send"])) {
-echo $name = $_POST ["yourName"];
-echo $email = $_POST["yourEmail"];
-echo  $message = $_POST["yourMessage"];
-$to = "lauravshaffer@gmail.com";
-$subject = "Message from Portfolio Contact Form";
-;
+/* Set e-mail recipient */
+$myemail = "portfolio.l@mailhero.io";
 
-mail($to,$subject,$name,$email,$message);
+/* Check all form inputs using check_input function */
+$name = check_input($_POST['inputName'], "Your Name");
+$email = check_input($_POST['inputEmail'], "Your E-mail Address");
+$message = check_input($_POST['inputMessage'], "Your Message");
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+show_error("Invalid e-mail address");
 }
+/* Let's prepare the message for the e-mail */
+
+$subject = "Message from Portfolio Contact Form";
+
+$message = "
+
+Someone has sent you a message using your contact form:
+
+Name: $name
+Email: $email
+Message:
+$message
+
+";
+
+/* Send the message using mail() function */
+mail($myemail, $subject, $message);
+
+/* Redirect visitor to the thank you page */
+header('Location: https://lauravshaffer.github.io');
+exit();
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
+}
+return $data;
+}
+
+function show_error($myError)
+{
 ?>
-<!DOCTYPE HTML>
 <html>
-<head>
-
-<title>Thanks!</title>
-
-</head>
 <body>
 
-<h1>Thanks</h1>
-<p>I will get back to you as soon as possible.</p>
-<p><a href="https://lauravshaffer.github.io">Back to my Portfolio</a></p>
+<p>Please correct the following error:</p>
+<strong><?php echo $myError; ?></strong>
+<p>Try again</p>
 
 </body>
 </html>
+<?php
+exit();
+}
+?>
